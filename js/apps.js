6fdb06b4c9ee74bca4914d418ae48d51f973b945
Ryohiro <87823201+Ryohiro@users.noreply.github.com>
2025-04-19 22:35:52 +0900

    変更

---
+++ js/apps.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const groupA = {
+        "a1": { name: "A-レイヤ1", color: "#FF6591" },
+        "a2": { name: "A-レイヤ2", color: "#FFB6C1" }
+    };
+    const groupB = {
+        "b1": { name: "B-レイヤ1", color: "#7C7AE0" },
+        "b2": { name: "B-レイヤ2", color: "#B0AEEE" }
+    };
+
+    buildLayerPanel("group-a", groupA);
+    buildLayerPanel("group-b", groupB);
+});
